b9d57976e3021e0c253f094376d0cb1d11be314f
Samiksha Walia <samikshawalia6903@gamil.com>
2024-09-27 15:51:53 +0530

    backend updated with rest of the graphs

--- myapp/views.py
+++ myapp/views.py
@@ -37,94 +37,31 @@ def generate_graph(request):
     graph_type = request.session.get('graph_type')
     file_path = request.session.get('file_path')
 
+    if not file_path:
+        return redirect('upload_file') 
+    
     dataframe = pd.read_csv(file_path)
     dataframe['DATETIME'] = pd.to_datetime(dataframe['DATE'] + ' ' + dataframe['TIME(UTC)'])
 
-    if graph_type == 'Yearly_mean_for_temp':
-        # Yearly mean for temperature
-        dataframe = dataframe.dropna(subset=['TEMP(C)'])
-        dataframe['YearMonth'] = dataframe['DATETIME'].dt.to_period('M')
-        monthly_mean_temp = dataframe.groupby('YearMonth')['TEMP(C)'].mean().reset_index()
-        monthly_stds = dataframe.groupby('YearMonth')['TEMP(C)'].std().reset_index()
+    plot_paths=[]
 
-        plt.figure(figsize=(12, 6))
-        plt.errorbar(monthly_mean_temp['YearMonth'].dt.to_timestamp(), monthly_mean_temp['TEMP(C)'], monthly_stds['TEMP(C)'], marker='o', linestyle='-')
-        plt.xlabel('Time')
-        plt.ylabel('Temperature (°C)')
-        plt.title('Monthly Mean Temperature Over Time')
-        plt.tight_layout()
+    if graph_type == 'Yearly_mean_for_temp':
+        plot_paths.append (generate_yearly_mean_for_temp(dataframe))
 
     elif graph_type == 'Yearly_mean_for_max_daily_temp':
-        # Yearly mean for max daily temperature
-        dataframe = dataframe.dropna(subset=['TEMP_MAX(C)'])
-        dataframe.set_index('DATETIME', inplace=True)
-        daily_max_temp = dataframe['TEMP_MAX(C)'].resample('D').max()
-        monthly_mean_max_temp = daily_max_temp.resample('M').mean()
-
-        plt.figure(figsize=(10, 5))
-        plt.plot(monthly_mean_max_temp, marker='o', linestyle='-', color='b')
-        plt.title('Mean of Maximum Daily Temperature per Month')
-        plt.xlabel('Month')
-        plt.ylabel('Mean Maximum Temperature (°C)')
-        plt.grid(True)
+        plot_paths.append(generate_yearly_mean_for_max_daily_temp(dataframe))
 
     elif graph_type == 'daily_max_temp':
-        # Daily maximum temperature
-        dataframe['YEAR'] = dataframe['DATETIME'].dt.year
-        dataframe['MONTH'] = dataframe['DATETIME'].dt.month
-        dataframe['DAY'] = dataframe['DATETIME'].dt.day
-        dataframe = dataframe[dataframe['YEAR'] != 2021]
-        daily_max_temps = dataframe.groupby(['YEAR', 'MONTH', 'DAY'])['TEMP(C)'].max().reset_index()
-        daily_max_temps.rename(columns={'TEMP(C)': 'MAX_TEMP'}, inplace=True)
-        years = daily_max_temps['YEAR'].unique()
-        months = daily_max_temps['MONTH'].unique()
-        month_names = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June', 7: 'July', 8: 'August', 9: 'September', 10: 'October', 11: 'November', 12: 'December'}
-
-        for year in sorted(years):
-            fig, axs = plt.subplots(6, 2, figsize=(15, 25), sharey=True)
-            fig.suptitle(f'Daily Maximum Temperatures for {year}', fontsize=16)
-            axs = axs.flatten()
-            for i, month in enumerate(sorted(months)):
-                ax = axs[i]
-                monthly_data = daily_max_temps[(daily_max_temps['YEAR'] == year) & (daily_max_temps['MONTH'] == month)]
-                if not monthly_data.empty:
-                    sns.lineplot(data=monthly_data, x='DAY', y='MAX_TEMP', marker='o', ax=ax)
-                ax.set_title(month_names[month])
-                ax.set_xlabel('Day of the Month')
-                ax.set_ylabel('Temperature (°C)')
-                ax.grid(True)
-            for i in range(len(months), len(axs)):
-                fig.delaxes(axs[i])
-            plt.tight_layout(rect=[0, 0, 1, 0.96])
-            plt.savefig(f'daily_max_temperatures_{year}.png')
-            plt.close(fig)
+        plot_paths.extend(generate_daily_max_temp(dataframe))
 
     elif graph_type == 'wind_rose':
-        # Wind rose graph
-        dataframe['Month'] = dataframe['DATETIME'].dt.month
-        wind_speed = dataframe['WIND_SPEED(kt)']
-        wind_direction = dataframe['WIND_DIR(deg)']
-        months = range(1, 13)
-        month_names = [calendar.month_name[month] for month in months]
+        plot_paths.extend(generate_wind_rose(dataframe))
 
-        for month, month_name in zip(months, month_names):
-            monthly_data = dataframe[dataframe['Month'] == month]
-            if not monthly_data.empty:
-                ax = WindroseAxes.from_ax()
-                ax.bar(monthly_data['WIND_DIR(deg)'], monthly_data['WIND_SPEED(kt)'], normed=True, opening=0.8, edgecolor='white', cmap=viridis)
-                ax.set_title(f'{month_name}')
-                ax.legend(loc='lower right', bbox_to_anchor=(1.12, 0), fontsize=8)
-                ax.set_rmax(20)
-                ax.set_yticks(range(0, 21, 5))
-                ax.set_yticklabels([str(i) for i in range(0, 21, 5)])
-                plt.savefig(f'wind_rose_{month_name}.png')
-                plt.close()
+    if plot_paths:  # Ensure plot_path is valid before storing
+        request.session['plot_paths'] = plot_paths
+    else:
+        request.session['plot_paths'] = None 
 
-    plot_path = os.path.join(settings.MEDIA_ROOT, 'plots', f'plot_{graph_type}.png')
-    plt.savefig(plot_path)
-    plt.close()
-
-    request.session['plot_path'] = plot_path
     return redirect('results')
 
 def generate_plots_and_csv(dataframe, y_col):
@@ -192,6 +129,112 @@ def generate_plots_and_csv(dataframe, y_col):
 
     return plot_path, combined_filename
 
+def generate_yearly_mean_for_temp(dataframe):
+    # Yearly mean for temperature
+    dataframe = dataframe.dropna(subset=['TEMP(C)'])
+    dataframe['YearMonth'] = dataframe['DATETIME'].dt.to_period('Y')
+    yearly_mean_temp = dataframe.groupby('YearMonth')['TEMP(C)'].mean().reset_index()
+
+    plt.figure(figsize=(10, 5))
+    plt.plot(yearly_mean_temp['YearMonth'].dt.to_timestamp(), 
+             yearly_mean_temp['TEMP(C)'], 
+             marker='o', linestyle='-', color='b')
+    plt.title('Yearly Mean Temperature')
+    plt.xlabel('Year')
+    plt.ylabel('Mean Temperature (°C)')
+    plt.grid(True)
+    plot_path = os.path.join(settings.MEDIA_ROOT, 'plots', 'yearly_mean_temp.png')
+    plt.savefig(plot_path)
+    plt.close()
+    return plot_path
+
+def generate_yearly_mean_for_max_daily_temp(dataframe):
+    # Yearly mean for max daily temperature
+    dataframe = dataframe.dropna(subset=['TEMP_MAX(C)'])
+    dataframe.set_index('DATETIME', inplace=True)
+    daily_max_temp = dataframe['TEMP_MAX(C)'].resample('D').max()
+    monthly_mean_max_temp = daily_max_temp.resample('M').mean()
+
+    plt.figure(figsize=(10, 5))
+    plt.plot(monthly_mean_max_temp.index, 
+             monthly_mean_max_temp.values, 
+             marker='o', linestyle='-', color='b')
+    plt.title('Mean of Maximum Daily Temperature per Month')
+    plt.xlabel('Month')
+    plt.ylabel('Mean Maximum Temperature (°C)')
+    plt.grid(True)
+    plot_path = os.path.join(settings.MEDIA_ROOT, 'plots', 'yearly_mean_max_daily_temp.png')
+    plt.savefig(plot_path)
+    plt.close()
+    return plot_path
+
+def generate_daily_max_temp(dataframe):
+    # Daily maximum temperature
+    dataframe['YEAR'] = dataframe['DATETIME'].dt.year
+    dataframe['MONTH'] = dataframe['DATETIME'].dt.month
+    dataframe['DAY'] = dataframe['DATETIME'].dt.day
+    dataframe = dataframe[dataframe['YEAR'] != 2021]
+    daily_max_temps = dataframe.groupby(['YEAR', 'MONTH', 'DAY'])['TEMP(C)'].max().reset_index()
+    daily_max_temps.rename(columns={'TEMP(C)': 'MAX_TEMP'}, inplace=True)
+    years = daily_max_temps['YEAR'].unique()
+    months = daily_max_temps['MONTH'].unique()
+    month_names = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 
+                   5: 'May', 6: 'June', 7: 'July', 8: 'August', 
+                   9: 'September', 10: 'October', 11: 'November', 12: 'December'}
+    
+    plot_paths=[]
+
+    for year in sorted(years):
+        fig, axs = plt.subplots(6, 2, figsize=(15, 25), sharey=True)
+        fig.suptitle(f'Daily Maximum Temperatures for {year}', fontsize=16)
+        axs = axs.flatten()
+        for i, month in enumerate(sorted(months)):
+            ax = axs[i]
+            monthly_data = daily_max_temps[(daily_max_temps['YEAR'] == year) & 
+                                            (daily_max_temps['MONTH'] == month)]
+            if not monthly_data.empty:
+                sns.lineplot(data=monthly_data, x='DAY', y='MAX_TEMP', marker='o', ax=ax)
+            ax.set_title(month_names[month])
+            ax.set_xlabel('Day of the Month')
+            ax.set_ylabel('Temperature (°C)')
+            ax.grid(True)
+        for i in range(len(months), len(axs)):
+            fig.delaxes(axs[i])
+        plt.tight_layout(rect=[0, 0, 1, 0.96])
+        plot_path = os.path.join(settings.MEDIA_ROOT, 'plots', f'daily_max_temperatures_{year}.png')
+        plt.savefig(plot_path)
+        plt.close()
+        plot_paths.append(plot_path)
+    return plot_paths
+
+def generate_wind_rose(dataframe):
+    # Wind rose graph
+    dataframe['Month'] = dataframe['DATETIME'].dt.month
+    wind_speed = dataframe['WIND_SPEED(kt)']
+    wind_direction = dataframe['WIND_DIR(deg)']
+    months = range(1, 13)
+    month_names = [calendar.month_name[month] for month in months]
+
+    plot_paths=[]
+
+    for month, month_name in zip(months, month_names):
+        monthly_data = dataframe[dataframe['Month'] == month]
+        if not monthly_data.empty:
+            ax = WindroseAxes.from_ax()
+            ax.bar(monthly_data['WIND_DIR(deg)'], monthly_data['WIND_SPEED(kt)'], 
+                   normed=True, opening=0.8, edgecolor='white', cmap=viridis)
+            ax.set_title(f'{month_name}')
+            ax.legend(loc='lower right', bbox_to_anchor=(1.12, 0), fontsize=8)
+            ax.set_rmax(20)
+            ax.set_yticks(range(0, 21, 5))
+            ax.set_yticklabels([str(i) for i in range(0, 21, 5)])
+            plot_path = os.path.join(settings.MEDIA_ROOT, 'plots', f'wind_rose_{month_name}.png')
+            plt.savefig(plot_path)
+            plt.close()
+            plot_paths.append(plot_path)
+    
+    return plot_paths
+
 def upload_file(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
@@ -219,7 +262,7 @@ def select_columns(request):
             y_axis = form.cleaned_data['y_axis']
             dataframe, _ = process_file(file_path)
             plot_path, csv_path = generate_plots_and_csv(dataframe, y_axis)
-            request.session['plot_path'] = plot_path
+            request.session['plot_paths'] = [plot_path]
             request.session['csv_path'] = csv_path
             return redirect('results')
     else:
@@ -228,11 +271,16 @@ def select_columns(request):
     return render(request, 'select_columns.html', {'form': form, 'file_path': file_path})
 
 def results(request):
-    plot_path = request.session.get('plot_path', '')
+    plot_paths = request.session.get('plot_paths', [])
     csv_path = request.session.get('csv_path', '')
-    plot_url = plot_path.replace(settings.MEDIA_ROOT, settings.MEDIA_URL)
-    csv_url = csv_path.replace(settings.MEDIA_ROOT, settings.MEDIA_URL)
-    return render(request, 'results.html', {'plot_url': plot_url, 'csv_url': csv_url})
+    plot_urls=[]
+
+    if plot_paths:
+        plot_urls=[path.replace(settings.MEDIA_ROOT,settings.MEDIA_URL)for path in plot_paths]
+
+    csv_url=csv_path.replace(settings.MEDIA_ROOT,settings.MEDIA_URL)if csv_path else None
+
+    return render(request, 'results.html', {'plot_urls': plot_urls, 'csv_url': csv_url})
 
 def download_file(request, filename):
     file_path = os.path.join(settings.MEDIA_ROOT, 'csv_outputs', filename)
