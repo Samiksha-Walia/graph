c8411a384889f488953cdba59d3a143fe955d7a3
Samiksha Walia <samikshawalia6903@gamil.com>
2024-12-27 17:26:10 +0530

    deployemnet update

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,12 @@
-echo  "BULID START"
-python -m pip install -r requirements.txt
-python manage.py collectstatic --noinput --clear 
+#!/bin/bash
+
+echo "BUILD START"
+
+# Install dependencies
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt || exit 1
+
+# Collect static files
+python3 manage.py collectstatic --noinput --clear || exit 1
+
 echo "BUILD END"
